26b60b45710944496ea400f98d9e3750c288c74e
Eliot Laidlaw <laidlawe18@barringtonschools.org>
2017-06-14 07:18:58 -0400

    Implemented basic tile map

---
+++ Super Duper Lamp/Assets/Scripts/GameData.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class GameData : MonoBehaviour {
+
+	public static GameData data;
+
+
+
+	// Use this for initialization
+	void Awake () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
