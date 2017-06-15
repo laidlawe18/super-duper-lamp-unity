161182bc3a9034d115e13605ba5b30121f3cb1b3
laidlawe18 <laidlawe18@barringtonschools.org>
2017-06-15 13:33:42 -0400

    Updated player movement

--- Super Duper Lamp/Assets/Scripts/Movement.cs
+++ Super Duper Lamp/Assets/Scripts/Movement.cs
@@ -18,6 +18,6 @@ public class Movement : MonoBehaviour {
         float moveHorizontal = Input.GetAxis("Horizontal");
         float moveVertical = Input.GetAxis("Vertical");
         Vector2 movement = new Vector2(moveHorizontal, moveVertical);
-        rb2d.AddForce(movement * speed);
+        rb2d.velocity = new Vector2(moveHorizontal * speed, rb2d.velocity.y);
 	}
 }
