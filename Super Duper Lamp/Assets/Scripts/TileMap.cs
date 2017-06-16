15c473f0e0493e89c2e1b810a7cd51545a136811
C-Toll <mcgartollc18@barringtonschools.org>
2017-06-16 10:43:49 -0400

    New Chunk Loading

--- Super Duper Lamp/Assets/Scripts/TileMap.cs
+++ Super Duper Lamp/Assets/Scripts/TileMap.cs
@@ -10,6 +10,12 @@ public class TileMap : MonoBehaviour {
     public const float texturePad = .1f;
 
     public GameObject chunk;
+    private int minX;
+    private int maxX;
+    private int minY;
+    private int maxY;
+    public const int loadSize = 10;
+    public const int deloadSize = 15;
 
     public int[,] map = {{1, 2, 1, 1, 1}, {1, 3, 0, 0, 1}, {1, 0, 2, 0, 1}, {1, 2, 0, 1, 1}, {1, 1, 3, 1, 3}};
 
@@ -26,7 +32,54 @@ public class TileMap : MonoBehaviour {
     // Update is called once per frame
     void Update()
     {
-
+        float heroX = GameObject.Find("Hero").transform.position.x;
+        float heroY = GameObject.Find("Hero").transform.position.y;
+        int heroXChunk = (int)(heroX / chunkSize);
+        int heroYChunk = (int)(heroY / chunkSize);
+        if(heroXChunk - minX < loadSize)
+        {
+            for(int i = heroXChunk-loadSize; i < minX; i++)
+            {
+                for(int j = minY; j < maxY; j ++)
+                {
+                    LoadChunk(i, j);
+                }
+            }
+            minX = heroXChunk - loadSize;
+        }
+        if (heroYChunk - minY < loadSize)
+        {
+            for (int i = heroYChunk - loadSize; i < minY; i++)
+            {
+                for (int j = minX; j < maxX; j++)
+                {
+                    LoadChunk(i, j);
+                }
+            }
+            minY = heroYChunk - loadSize;
+        }
+        if (maxY - heroYChunk < loadSize)
+        {
+            for (int i = maxY; i < heroYChunk + loadSize; i++)
+            {
+                for (int j = minX; j < maxX; j++)
+                {
+                    LoadChunk(i, j);
+                }
+            }
+            maxY = heroYChunk + loadSize;
+        }
+        if (maxX - heroXChunk < loadSize)
+        {
+            for (int i = maxX; i < heroXChunk + loadSize; i++)
+            {
+                for (int j = minY; j < maxY; j++)
+                {
+                    LoadChunk(i, j);
+                }
+            }
+            maxX = heroXChunk + loadSize;
+        }
     }
 
     void BuildMap()
@@ -56,11 +109,11 @@ public class TileMap : MonoBehaviour {
 	
 	void BuildChunks()
     {
-        int xChunks = 80;
-        int yChunks = 30;
+        int xChunks = map.GetLength(0)/chunkSize;
+        int yChunks = map.GetLength(1)/chunkSize;
 
         chunks = new GameObject[xChunks, yChunks];
-        for (int i = 0; i < xChunks; i++)
+        /*for (int i = 0; i < xChunks; i++)
         {
             for (int j = 0; j < yChunks; j++)
             {
@@ -68,7 +121,15 @@ public class TileMap : MonoBehaviour {
                 chunks[i, j].transform.SetParent(this.gameObject.transform);
                 chunks[i, j].GetComponent<TileMapChunk>().SetXandY(i * chunkSize, j * chunkSize);
             }
+        }*/
+    }
+    void LoadChunk (int i, int j)
+    {
+        if (!(i < 0 || i >= chunks.GetLength(0) || j < 0 || j >= chunks.GetLength(1)))
+        {
+            chunks[i, j] = Instantiate(chunk, new Vector3(i * chunkSize, j * chunkSize), Quaternion.identity) as GameObject;
+            chunks[i, j].transform.SetParent(this.gameObject.transform);
+            chunks[i, j].GetComponent<TileMapChunk>().SetXandY(i * chunkSize, j * chunkSize);
         }
     }
-    
 }
